35bf8053b663c2b1be716afd578a8f3437e27a6a
Fitsum <fitbez@gmail.com>
2020-08-24 20:16:13 -0400

    Updates

---
+++ precache-manifest.111a07528726c5076abc756d773e7cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d044ce8b6a8cfde9294b01bd02764982",
+    "url": "/searched-country/index.html"
+  },
+  {
+    "revision": "3dfeffa4c9d6ff4e00c5",
+    "url": "/searched-country/static/css/main.4a395930.chunk.css"
+  },
+  {
+    "revision": "5b8b74834688fc1214b1",
+    "url": "/searched-country/static/js/2.9e413b73.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/searched-country/static/js/2.9e413b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dfeffa4c9d6ff4e00c5",
+    "url": "/searched-country/static/js/main.186f2477.chunk.js"
+  },
+  {
+    "revision": "53962871d76b28c30ecd",
+    "url": "/searched-country/static/js/runtime-main.2c6159b9.js"
+  }
+]);
